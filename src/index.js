6c7734528d876aabd35fdbcb71a5b4ee37e082d4
ando.yuma <foymitsu-1115pye@outlook.jp>
2020-04-27 16:40:07 +0900

    三目並べチュートリアル完成

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import Game from './Game'
+//add
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+// const store=createStore(Reduces);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
